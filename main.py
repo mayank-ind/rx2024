c3f6fe6eba9ba69c2d19e736ac14c06096cb4c7c
ABHIMANHYU <97294034+ABHIMANYU509@users.noreply.github.com>
2023-07-16 13:21:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -122,6 +122,8 @@ async def account_login(bot: Client, m: Message):
     else:
         thumb == "no"
 
+    failed_url = ""
+  
     if len(links) == 1:
         count = 1
     else:
@@ -197,12 +199,14 @@ async def account_login(bot: Client, m: Message):
                     count += 1
 
             except Exception as e:
-                await m.reply_text(f"#Failed {name} {url}")
+                failed_url += f"{name} {url}"
                 count += 1
                 continue
 
     except Exception as e:
         await m.reply_text(e)
+        f.write(failed_url)
+        await m.reply_document("failed_url")
     await m.reply_text("Batch Successfully Completed ☺️")
 
 
