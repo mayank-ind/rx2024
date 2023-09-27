11e9e1e7b0667a63ef0964e19e9d5901e9d4b62c
Your Abbu From TG <97294034+ABHIMANYU509@users.noreply.github.com>
2023-09-27 14:48:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,12 @@
 import os
 
-API_ID = API_ID = 10010060
-
-API_HASH = os.environ.get("API_HASH", "807495c78593302dd5008b4775e9b01b")
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6101033089:AAEZ6ObvNn87UusS3YDpj9cTQejRs_cVXdc")
-
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6101033089:AAEZ6ObvNn87UusS3YDpj9cTQejRs_cVXdc"")
+API_ID = int(os.environ.get("API_ID", "10010060"))
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
-
-OWNER = int(os.environ.get("OWNER", 1503980120))
-
-LOG = -1001927764573
-
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1503980120").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-ADMINS.append(OWNER)
-
+FR_TOKEN = os.environ.get("FR_TOKEN", "f0907b55-922e-414a-8d21-47c1887bb90d"))
+API_KEY = os.environ.get("API_KEY", " "))
+API_HASH = os.environ.get("API_HASH", "807495c78593302dd5008b4775e9b01b")
+ADMINS = int(os.environ.get("ADMINS", "1503980120"))
+GROUPS = [int(chat) for chat in os.getenv("GROUPS", "-1001927764573").split(",") if chat != ""]
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
