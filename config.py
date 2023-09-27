9274e2df6b3c60c3c254042baa069d1bafc5ad45
Your Abbu From TG <97294034+ABHIMANYU509@users.noreply.github.com>
2023-09-27 14:54:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6101033089:AAEZ6ObvNn87UusS3YDpj9cTQejRs_cVXdc"")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6101033089:AAEZ6ObvNn87UusS3YDpj9cTQejRs_cVXdc")
 API_ID = int(os.environ.get("API_ID", "10010060"))
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 FR_TOKEN = os.environ.get("FR_TOKEN", "f0907b55-922e-414a-8d21-47c1887bb90d"))
